7cdf812ce211e9a3042b10390b7f778912f898c5
Tim Harris <raymondtimothyharris@gmail.com>
2020-07-08 23:24:37 -0700

    NewletterUsers Query
    Data is fetched for query. Need to map data to the correct object.

--- src/resolvers.js
+++ src/resolvers.js
@@ -8,7 +8,7 @@ const documentClient = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
 const resolvers = {
     Query : {
         newsletterUser : async (_,{emailAddress}, {}) => {
-            console.log(emailAddress);
+            //console.log(emailAddress);
             let responseBody = "";
         
             let statusCode = 0;
@@ -20,7 +20,7 @@ const resolvers = {
             }
             try{
                 const data = await documentClient.get(params).promise();
-                console.log(data.Item);
+                //console.log(data.Item);
                 responseBody = data.Item;
                 statusCode = 200;
             } catch(err){
@@ -38,7 +38,41 @@ const resolvers = {
             }
 
             return {firstname : responseBody.Firstname, lastname : responseBody.Lastname, emailAddress : responseBody.Email}
+        },
+        newsletterUsers : async(_,{},{}) => {
+            let responseBody = [];
+            let statusCode = 0;
+
+            const params = {
+                TableName: "BadMagic_NewsletterUsers",
+                Limit:3
+            }
+            try{
+                const data = await documentClient.scan(params).promise();
+                console.log(data.Items);
+                //Map array to array of NewsletterUsers
+
+                responseBody = data.Items.map({Firstname, Lastname, Email})
+                console.log(responseBody)
+                statusCode = 200;
+            }   catch (err){
+                console.log(err)
+                responseBody = `Unable to get Items`
+                statusCode = 403
+            }
+
+            const response = {
+                statusCode:statusCode,
+                headers:{
+                    "myHeader": "test",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                body : responseBody
+            }
+
+            return responseBody;
         }
+
     },
 };
 
